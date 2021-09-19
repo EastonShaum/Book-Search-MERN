44a1015ac0b12883be80b18971cde593835a6ea4
Easton Shaum <Eastonshaum@gmail.com>
2021-09-18 19:41:21 -0600

    completed: server/server.js, auth.js, schema index.js, typeDefs, resolvers. Started: client queries, mutations, App.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,89 @@
+const { AuthenticationError } = require('apollo-server-express')
+const { Book, User } = require('../models')
+const { signToken } = require('../utils/auth')
+
+// GraphQL Not working, how to remove book, queries in graphQl
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                .populate('savedBooks');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in')
+        },
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+            .populate('savedBooks');
+        },
+        users: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('savedBooks');
+        },
+        books: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params)
+        },
+        book: async (parent, { bookId }) => {
+            return Book.findOne({ bookId })
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user}
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw =await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token =signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if(context.user) {
+                const book = await Book.create({ ...args, username: context.user.username})
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { books: book.bookId }},
+                    { new: true }
+                );
+                return book;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        removeBook: async (parent, args, context) => {
+            if(context.user) {
+                const book = await Book.create({ ...args, username: context.user.username})
+                await User.findByIdAndDelete(
+                    { _id: context.user._id },
+                    { $delete: { books: book.bookId }},
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    }
+}
+
+
+module.exports = resolvers;
